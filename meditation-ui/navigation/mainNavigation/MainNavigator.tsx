5330afd147f820ec97eb7ba2483eb2715d383263
BenNeighbour <benneighbour007@gmail.com>
2021-10-28 23:21:06 +0100

    Temporarily mapping other pages to empty views

--- meditation-ui/navigation/mainNavigation/MainNavigator.tsx
+++ meditation-ui/navigation/mainNavigation/MainNavigator.tsx
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React, { FunctionComponent } from "react";
+import { View } from 'react-native';
 import { Home } from "../../screens/home";
 import { Meditate } from '../../screens/meditate';
 import TabBar from "./tabBar/TabBar";
@@ -12,10 +13,10 @@ const MainNavigator: FunctionComponent<Props> = () => {
     return (
         <Tab.Navigator tabBar={(props: any) => <TabBar {...props} />}>
             <Tab.Screen options={{ headerShown: false }} name="Home" component={Home} />
-            <Tab.Screen options={{ headerShown: false }} name="Sleep" component={Home} />
+            <Tab.Screen options={{ headerShown: false }} name="Sleep" component={View} />
             <Tab.Screen options={{ headerShown: false }} name="Meditate" component={Meditate} />
-            <Tab.Screen options={{ headerShown: false }} name="Music" component={Home} />
-            <Tab.Screen options={{ headerShown: false }} name="Asfar" component={Home} />
+            <Tab.Screen options={{ headerShown: false }} name="Music" component={View} />
+            <Tab.Screen options={{ headerShown: false }} name="Asfar" component={View} />
         </Tab.Navigator>
     );
 };
