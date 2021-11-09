1aa08ddb2b72af986b5e81638863dc782ebd141e
BenNeighbour <benneighbour007@gmail.com>
2021-11-09 16:49:42 +0000

    Patching up some deprecation issues

--- music-service/src/main/java/io/benneighbour/silentmoon/musicservice/MusicServiceApplication.java
+++ music-service/src/main/java/io/benneighbour/silentmoon/musicservice/MusicServiceApplication.java
@@ -3,10 +3,10 @@ package io.benneighbour.silentmoon.musicservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableResourceServer
 @SpringBootApplication
--- music-service/src/main/java/io/benneighbour/silentmoon/musicservice/config/SecurityConfig.java
+++ music-service/src/main/java/io/benneighbour/silentmoon/musicservice/config/SecurityConfig.java
@@ -1,26 +1,33 @@
 package io.benneighbour.silentmoon.musicservice.config;
 
 import lombok.RequiredArgsConstructor;
-import java.util.Collections;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
+
+import java.util.Collections;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends ResourceServerConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final ResourceServerProperties resource;
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.cors();
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/api/public/**").permitAll()
